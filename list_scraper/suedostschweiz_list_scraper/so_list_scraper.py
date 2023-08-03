993cbf53632c62a09b67c2feefb146569e8e1b2b
Yanik Kipfer <kipfer@soziologie.uzh.ch>
2023-08-03 11:02:01 +0200

    initial commit

---
+++ list_scraper/suedostschweiz_list_scraper/so_list_scraper.py
@@ -0,0 +1,13 @@
+# Tasks: Got to https://www.suedostschweizjobs.ch and create a list-scraper that scrapes the following ad details:
+#   - ID
+#   - URL
+#   - Title
+#   - Company name
+#   - Location
+#   - Date Published
+# Save the extracted data to a CSV file.
+# Use the requests and BeautifulSoup modules to scrape the data.
+
+
+if __name__ == "__main__":
+    pass
